70af25e22aef2dcbb40f35faefda7594b0ed334e
JT <jeweltail@cogeco.ca>
2024-02-15 18:54:03 -0800

    l

--- src/main/java/frc/robot/Swerve/commands/targetAim.java
+++ src/main/java/frc/robot/Swerve/commands/targetAim.java
@@ -1,10 +1,12 @@
 package frc.robot.Swerve.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Swerve.Subsystem.Swerve;
 
 public class targetAim extends Command {
     boolean isFinished; 
-    
+    private Swerve s_Swerve; 
+
 
     @Override 
     public void initialize() {
