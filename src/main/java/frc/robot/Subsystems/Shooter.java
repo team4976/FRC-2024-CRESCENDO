a1aa9e3cc04b38ec0143852f82711c0ad8d67cfa
JT <jeweltail@cogeco.ca>
2024-02-20 16:56:17 -0800

    speakerAim and Shooter

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -1,8 +1,41 @@
 package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType; 
 
 public class Shooter extends SubsystemBase {
-    //
+    //define motors. yeah. 
+    private final CANSparkMax shSpark = new CANSparkMax(0,MotorType.kBrushless); 
+    private final TalonFX shIndexer = new TalonFX(0); 
+    private final CANSparkMax aimSpark = new CANSparkMax(0,MotorType.kBrushless); 
+    //define whatever angles the shooter
+    //I hope someone else codes the elevator i don't know how to code the pistons and don't want to learn
+    //now that I've said that it'll end up being me who does that won't it. sigh. 
+
+    //ENCODERS?? its a tuesday problem
+
+    double angle; 
+
+    public Shooter() {
+        
+    }
+
+    public void shAim(double a){
+        //encoder?? ENCODES?? 
+        //point at A value
+        
+    }
+    
+    public void shoot(){
+        //index
+        //shoot
+    }
+
     
 }
+//raise shooter
+//angle shooter
+//index [note: index also used to intake notes] [heh. index. intake. badum tiss]
+//shoot
--- src/main/java/frc/robot/Swerve/commands/speakerAim.java
+++ src/main/java/frc/robot/Swerve/commands/speakerAim.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Swerve.Subsystem.Swerve;
+import frc.robot.Subsystems.Shooter;
 import frc.robot.Subsystems.limelight;
 
 public class speakerAim extends Command {
@@ -16,11 +17,19 @@ public class speakerAim extends Command {
     private double distMax = distSetpoint + 6;
     private double hMin = hSetpoint - 6;
     private double hMax = hSetpoint + 6;   
+    private double speakerTarget; 
     private Swerve a_Swerve; 
+    private Shooter a_Shooter; 
     private limelight a_l;
     PIDController pidT = new PIDController(0.015, 0, 0);
     PIDController pidF = new PIDController(0.015, 0, 0);
     PIDController pidH = new PIDController(0.015, 0, 0); 
+
+    public speakerAim(Swerve s){
+        a_Swerve = s; 
+        addRequirements(s);
+        //do I need this? I just need swerve linked to the robotcontainer class. 
+    }
 
     @Override 
     public void initialize() {
@@ -32,12 +41,14 @@ public class speakerAim extends Command {
         pidF.setTolerance(5.0);
         pidH.setTolerance(5.0); 
         //set shooter to a point we'll just have to determine with trial and error ig 
+        speakerTarget = 3; 
+        //NOTE: ADD CODE TO DETERMINE WHICH TAG BY ALLIANCE COLOUR LATER
     }
 
     @Override 
     public void execute() {
         if (a_l.ID() == 3){
-            if (a_l.THor() < distMax && a_l.THor() > distMin){
+            if (a_l.THor() < distMax && a_l.THor() > distMin && a_l.X() < hMax && a_l.X() > hMin){
                 botPositioned = true; 
             }
             //insert similar gate to make shooter aimed true (provided ll on shooter)
@@ -48,8 +59,8 @@ public class speakerAim extends Command {
                 - pidT.calculate(a_l.X(), hSetpoint), false, true);
             }
             if (botPositioned){
-                //this is where I'd put my shoot command
-                //IF I HAD ONE
+                a_Shooter.shoot(); 
+                isFinished = true; 
             }
         }
         else {
