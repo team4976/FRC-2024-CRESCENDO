5a9afaf0f66dbc1020eaecd04e5d8846b9c8f58c
straight-from-hell <rachelecarbin@gmail.com>
2024-02-20 19:18:52 -0500

    made auto folder for commands (also other things but I don't remember)

--- src/main/java/frc/robot/Autonomous/speakerAim.java
+++ src/main/java/frc/robot/Autonomous/speakerAim.java
@@ -1,4 +1,4 @@
-package frc.robot.Swerve.commands;
+package frc.robot.Autonomous;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
--- src/main/java/frc/robot/Subsystems/intake.java
+++ src/main/java/frc/robot/Subsystems/intake.java
@@ -1,7 +1,12 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class intake extends SubsystemBase{
-    
+    private final TalonSRX runIntake = new TalonSRX(0);
+
+    public intake(){
+        
+    }
 }
