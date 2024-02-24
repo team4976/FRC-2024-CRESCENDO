a7055d3388b316fcae8b46e649b57e9a676bc1b0
straight-from-hell <rachelecarbin@gmail.com>
2024-02-24 09:58:21 -0500

    elevator things

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,15 +1,18 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public final class RobotConstants {
 //motors n whatnot
     //shooter
     //intake
-    public static final TalonSRX m_IntakeTalon = new TalonSRX(0);
+    public static final TalonSRX m_IntakeTalon = new TalonSRX(47);
     //elevator
-    public static final TalonSRX m_ElevatorTalon = new TalonSRX(0);
-    //sim
+    public static final TalonSRX m_ElevatorTalon = new TalonSRX(45);
+    public static final VictorSPX m_ElevatorVictor = new VictorSPX(46);
     //idk is there anything else we need?
 //variables
+    public static boolean elevator_up = false;
+    public static boolean shooter_up = false;
 }
--- src/main/java/frc/robot/Subsystems/elevator.java
+++ src/main/java/frc/robot/Subsystems/elevator.java
@@ -3,7 +3,20 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //motor import
+import static frc.robot.RobotConstants.m_ElevatorTalon;
+import static frc.robot.RobotConstants.m_ElevatorVictor;
+import static frc.robot.RobotConstants.elevator_up;
+import frc.robot.RobotConstants;
 
-public class elevator {
-    
+public class elevator extends SubsystemBase{
+    public void elevate(){
+        m_ElevatorTalon.set(ControlMode.PercentOutput,0.4);
+        m_ElevatorVictor.follow(m_ElevatorTalon);
+        elevator_up = true;
+    }
+    public void reverse(){
+        m_ElevatorTalon.set(ControlMode.PercentOutput,-0.4);
+        m_ElevatorVictor.follow(m_ElevatorTalon);
+        elevator_up = false;
+    }
 }
