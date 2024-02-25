74255c4391806bb611ec6b8765ec700a7f62fd00
straight-from-hell <rachelecarbin@gmail.com>
2024-02-25 13:45:04 -0500

    fixed findNote

--- src/main/java/frc/robot/Autonomous/findNote.java
+++ src/main/java/frc/robot/Autonomous/findNote.java
@@ -1,21 +1,8 @@
 package frc.robot.Autonomous;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.swing.text.Position;
-
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Swerve.Constants;
 import frc.robot.Swerve.Subsystem.Swerve;
 import frc.robot.Subsystems.intake;
 import frc.robot.Subsystems.limelight;
@@ -57,19 +44,7 @@ public class findNote extends Command{
         //System.out.println(noteLocation[index][0]);
         //System.out.println(noteLocation[index][1]);
         
-        TrajectoryConfig config = 
-            new TrajectoryConfig(
-            Constants.AutoConstants.kMaxSpeedMetersPerSecond,
-            Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            .setKinematics(Constants.Swerve.swerveKinematics);
-        Trajectory goToNote = 
-            TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // navigate to a note
-            List.of(new Translation2d(diffH, diffV)),
-            new Pose2d(0, 0, new Rotation2d(0)),
-            config);
+        a_Swerve.drive(new Translation2d(diffH, diffV), 0, true, true);
     }
 
     public void end(){
