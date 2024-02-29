a7c07eaedb9a6cc8c73491e03c4c5c01f891c129
Vectron0 <147773732+Vectron0@users.noreply.github.com>
2024-02-29 18:51:01 -0500

    Create goToPos.java
    this is the go to position command, i have not been able to test it but it should work.

---
+++ src/main/java/frc/robot/Autonomous/goToPos.java
@@ -0,0 +1,71 @@
+package frc.robot.Autonomous;//geting a ring auto, this should work, have not had time to test.
+
+import frc.robot.Subsystems.limelight;
+import frc.robot.Swerve.Constants;
+import frc.robot.Swerve.Subsystem.Swerve;
+import frc.robot.Commands.AutoSonar;
+
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+
+public class goToRing extends SequentialCommandGroup {
+    public goToRing(Swerve s_Swerve, limelight limelight){
+        TrajectoryConfig config =
+            new TrajectoryConfig(
+                    Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+                    Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                .setKinematics(Constants.Swerve.swerveKinematics);
+
+        // An example trajectory to follow.  All units in meters.
+        Trajectory exampleTrajectory =
+            TrajectoryGenerator.generateTrajectory(
+                // Start at the origin facing the +X direction
+                new Pose2d(limelight.botpose()[0], limelight.botpose()[1], new Rotation2d(0)),
+                // Pass through these two interior waypoints, making an 's' curve path
+                List.of(),
+                // End 3 meters straight ahead of where we started, facing forward
+                new Pose2d(limelight.botpose()[0], limelight.botpose()[1], new Rotation2d(180)),
+                config);
+
+
+                
+
+
+        var thetaController =
+            new ProfiledPIDController(
+                Constants.AutoConstants.kPThetaController, 0, 0, Constants.AutoConstants.kThetaControllerConstraints);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        SwerveControllerCommand swerveControllerCommand =
+            new SwerveControllerCommand(
+                exampleTrajectory,
+                s_Swerve::getPose,
+                Constants.Swerve.swerveKinematics,
+                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+                thetaController,
+                s_Swerve::setModuleStates,
+                s_Swerve);
+
+
+        addCommands(
+            new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialPose())),
+            swerveControllerCommand
+        );
+    }
+}
+
+
+
